173523056b06b108399d4ca64419cad9e018b438
Glen Olinger <glen.olinger@gmail.com>
2017-04-08 08:06:38 -0500

    Removed a debugging console log statement.

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ WARNING!!! WARNING!!!
 
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", 
                "Paula", "Laura", "Jim"];
-  console.log( "Hello. We have a IIFE function!");
+  //console.log( "Hello. We have a IIFE function!");
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
